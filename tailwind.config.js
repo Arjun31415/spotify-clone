77372a5ef02f91db7849884100319a2b8e6a9a84
Arjun31415 <arjunp0710@gmail.com>
2021-10-23 17:31:01 +0530

    added custom colors and backgrounds

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,32 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ['"spotify-circular"', "Helvetica", "Arial", "sans-serif"],
+    },
+    extend: {
+      backgroundImage: {},
+      colors: {
+        spotifyBlue: "#2941ab",
+        spotifyGreen: "#1ed760",
+        spotifyLightGreen1: "#2de26d",
+        spotifyLightGreen2: "#43e57d",
+      },
+      backgroundSize: {
+        170: "170%",
+      },
+      backgroundPosition: {
+        customHome: " 44% 7%",
+      },
+      fontSize: {
+        "10xl": "10rem",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
