0c83ac1854c4843b304364eccc895c0a20638f3f
Arjun31415 <arjunp0710@gmail.com>
2021-10-24 09:09:28 +0530

    Bug fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,38 @@
 module.exports = {
-  mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    fontFamily: {
-      sans: ['"spotify-circular"', "Helvetica", "Arial", "sans-serif"],
-    },
-    extend: {
-      backgroundImage: {},
-      colors: {
-        spotifyBlue: "#2941ab",
-        spotifyGreen: "#1ed760",
-        spotifyLightGreen1: "#2de26d",
-        spotifyLightGreen2: "#43e57d",
-      },
-      backgroundSize: {
-        170: "170%",
-      },
-      backgroundPosition: {
-        customHome: " 44% 7%",
-      },
-      fontSize: {
-        "10xl": "10rem",
-      },
-    },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+	mode: "jit",
+	purge: [
+		"./pages/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+		"./Components/**/*.{js,ts,jsx,tsx}",
+		"./public/**/*.html",
+		"./src/**/*.{js,jsx,ts,tsx,vue}",
+	],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		fontFamily: {
+			sans: ['"spotify-circular"', "Helvetica", "Arial", "sans-serif"],
+		},
+		extend: {
+			backgroundImage: {},
+			colors: {
+				spotifyBlue: "#2941ab",
+				spotifyGreen: "#1ed760",
+				spotifyLightGreen1: "#2de26d",
+				spotifyLightGreen2: "#43e57d",
+			},
+			backgroundSize: {
+				170: "170%",
+			},
+			backgroundPosition: {
+				customHome: " 44% 7%",
+			},
+			fontSize: {
+				"10xl": "10rem",
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
 };
