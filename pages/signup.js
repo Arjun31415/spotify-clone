8bb83ebac56e11f16c384085f9803228298679d3
Arjun31415 <arjunp0710@gmail.com>
2021-11-09 09:55:17 +0530

    Create singup page and component

---
+++ pages/signup.js
@@ -0,0 +1,10 @@
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Signup from "../components/Signup";
+export default function SignupPage() {
+	return (
+		<>
+			<Signup />
+		</>
+	);
+}
