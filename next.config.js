978cb7dd9c43644327f5a79858a933dcc395b60d
Arjun31415 <arjunp0710@gmail.com>
2021-12-24 22:03:08 +0530

    Typescript Update

--- next.config.js
+++ next.config.js
@@ -5,6 +5,20 @@ module.exports = {
 	images: {
 		domains: ["i.scdn.co"],
 	},
+	webpack(config, options) {
+		const { dir, defaultLoaders } = options;
+		config.resolve.extensions.push(".ts", ".tsx");
+		config.module.rules.push({
+			test: /\\.+(ts|tsx)$/,
+			include: [dir],
+			exclude: /node_modules/,
+			use: [
+				defaultLoaders.babel,
+				{ loader: "ts-loader", options: { transpileOnly: true } },
+			],
+		});
+		return config;
+	},
 	async headers() {
 		return [
 			{
