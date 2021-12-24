5f20305e62dd6a723cde027ad3962f19d507e9ad
Arjun31415 <arjunp0710@gmail.com>
2021-12-24 23:00:31 +0530

    bug fixes

--- components/Login.tsx
+++ components/Login.tsx
@@ -6,14 +6,6 @@ import styles from "./Signup.module.css";
 export default function Login() {
 	return (
 		<div className={`flex flex-row justify-center ${styles.mt2} `}>
-			{/* scripts for recaptcha */}
-			{/* <Script src="/reCaptcha.js"></Script>
-			<script
-				src="https://www.google.com/recaptcha/api.js?onload=onloadCallback&render=explicit"
-				async
-				defer
-			></script> */}
-
 			<div className={`flex flex-col`}>
 				<div className={`bg-transparent flex justify-center `}>
 					<Brand textColor="text-black" height="6vh" />
